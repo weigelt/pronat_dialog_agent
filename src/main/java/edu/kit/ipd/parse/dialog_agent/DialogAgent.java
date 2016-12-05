6e451a228489cbde64e9d0cfb8ce973f3744558e
Mario <oiram24186@yahoo.de>
2016-12-05 16:50:36 +0100

    Initial commit

---
+++ src/main/java/edu/kit/ipd/parse/dialog_agent/DialogAgent.java
@@ -0,0 +1,28 @@
+package edu.kit.ipd.parse.dialog_agent;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import edu.kit.ipd.parse.luna.ILuna;
+import edu.kit.ipd.parse.luna.agent.AbstractAgent;
+import edu.kit.ipd.parse.luna.event.AbortEvent;
+import edu.kit.ipd.parse.luna.event.IEvent;
+import edu.kit.ipd.parse.luna.event.UpdateEvent;
+import edu.kit.ipd.parse.luna.graph.IGraph;
+
+public class DialogAgent extends AbstractAgent {
+
+	/**
+	 * execution takes part here
+	 */
+	// TODO: public?
+	protected void exec() {
+
+	}
+
+	@Override
+	public void init() {
+		// TODO Auto-generated method stub
+
+	}
+}
